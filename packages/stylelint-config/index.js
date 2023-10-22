9c7c25d0ea2b9888b738ceb06ca4cacf3732a6e7
advl <sonora@779.mx>
2023-10-22 22:47:42 +0000

    Minor changes

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -5,5 +5,10 @@ module.exports = {
   ],
   rules:{
     indentation:2,
+    // For more legibility where `@use` and `@include` follow each other
+    'at-rule-empty-line-before':['always', {
+      except       :['after-same-name', 'inside-block'], 
+      ignoreAtRules:['include', 'mixin', 'function'],    
+    }],
   }
 }
