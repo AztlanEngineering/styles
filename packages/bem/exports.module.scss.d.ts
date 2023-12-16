5b2916aadc3765cd0920a14661345f87999f0278
advl <700@779.mx>
2023-12-16 16:31:49 +0000

    Type generation for bem

---
+++ packages/bem/exports.module.scss.d.ts
@@ -0,0 +1,23 @@
+declare module 'exports.module.scss' {
+  "base": string;
+  "elementContent": string;
+  "elementHeading": string;
+  "elementItem": string;
+  "elementSection": string;
+  "elementSubtitle": string;
+  "modifierActive": string;
+  "modifierAnimated": string;
+  "modifierAttached": string;
+  "modifierBlur": string;
+  "modifierClear": string;
+  "modifierCompact": string;
+  "modifierError": string;
+  "modifierGhost": string;
+  "modifierGradient": string;
+  "modifierImage": string;
+  "modifierOpen": string;
+  "modifierSimple": string;
+  "modifierSpaced": string;
+  "modifierUnderlined": string;
+  "wrapperGroup": string;
+}
---
+++ packages/bem/generate-types.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const fs = require('fs');
+
+const scssFilePath = 'exports.module.scss'; // Adjust this path accordingly
+const outputFilePath = 'exports.module.scss.d.ts'; // Adjust the output path
+
+const scssContent = fs.readFileSync(scssFilePath, 'utf8');
+const lines = scssContent.split('\n');
+
+let inExportBlock = false;
+const typeDeclarations = [];
+
+lines.forEach((line, index) => {
+  if (line.includes(':export')) {
+    inExportBlock = true;
+  } else if (inExportBlock) {
+    const trimmedLine = line.trim();
+    if (trimmedLine && !trimmedLine.startsWith('//') && trimmedLine.includes(':')) {
+      const [variable,] = trimmedLine.split(':');
+      const variableName = variable.trim();
+      typeDeclarations.push(`  "${variableName}": string;`);
+    }
+  }
+});
+
+const tsDeclaration = `declare module '${scssFilePath}' {\n${typeDeclarations.join('\n')}\n}\n`;
+
+fs.writeFileSync(outputFilePath, tsDeclaration, 'utf8');
